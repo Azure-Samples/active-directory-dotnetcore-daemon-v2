8e731ccfa08bf4e4f03bce735d5138727678510c
Tiago Brenck <v-tibre@microsoft.com>
2019-09-02 02:46:23 -0700

    Sample: Daemon calling protected api (#32)
    * Skeleton projects
    
    * appsettings with placeholders
    
    * Web api code
    
    * daemon consuming web api
    
    * App Creation scripts
    
    * Readme file
    
    Minor refactoring
    
    * updated svg
    
    * Addressed PR suggestions.
    
    Added validation on the ActionResult level
    
    * Addressed PR comments
    
    * Fixed launchSettings
    
    * updated Cleanup.ps1
    
    * README indentation
    
    * The endpoint property is just used internally by the code generator to check if this is a v1 or v2 sample. The text AAD v2.0 is not printed anywhere
    
    * Added AppRoles on sample.json
    
    * Updated ps1 according to Generator
    
    * fixed scenario images
    
    * fixed scenario images
    
    * svg images
    
    * using .png image for topology instead of svg
    
    * Suggestion for the initial readme, showing the tutorial structure
    
    * updated topology img
    
    * Moved the first step into the folder 1-Call-MSGraph
    
    * updated to msal 4.3.1
    
    * Addressed missing commits
    
    * Added shell command to navigate to step 1 folder

---
+++ 2-Call-OwnApi/daemon-console/AuthenticationConfig.cs
@@ -0,0 +1,118 @@
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2015 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace daemon_console
+{
+    /// <summary>
+    /// Description of the configuration of an AzureAD public client application (desktop/mobile application). This should
+    /// match the application registration done in the Azure portal
+    /// </summary>
+    public class AuthenticationConfig
+    {
+        /// <summary>
+        /// instance of Azure AD, for example public Azure or a Sovereign cloud (Azure China, Germany, US government, etc ...)
+        /// </summary>
+        public string Instance { get; set; } = "https://login.microsoftonline.com/{0}";
+
+        /// <summary>
+        /// The Tenant is:
+        /// - either the tenant ID of the Azure AD tenant in which this application is registered (a guid)
+        /// or a domain name associated with the tenant
+        /// - or 'organizations' (for a multi-tenant application)
+        /// </summary>
+        public string Tenant { get; set; }
+
+        /// <summary>
+        /// Guid used by the application to uniquely identify itself to Azure AD
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// URL of the authority
+        /// </summary>
+        public string Authority
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, Instance, Tenant);
+            }
+        }
+
+        /// <summary>
+        /// Client secret (application password)
+        /// </summary>
+        /// <remarks>Daemon applications can authenticate with AAD through two mechanisms: ClientSecret
+        /// (which is a kind of application password: this property)
+        /// or a certificate previously shared with AzureAD during the application registration 
+        /// (and identified by the CertificateName property belows)
+        /// <remarks> 
+        public string ClientSecret { get; set; }
+
+        /// <summary>
+        /// Name of a certificate in the user certificate store
+        /// </summary>
+        /// <remarks>Daemon applications can authenticate with AAD through two mechanisms: ClientSecret
+        /// (which is a kind of application password: the property above)
+        /// or a certificate previously shared with AzureAD during the application registration 
+        /// (and identified by this CertificateName property)
+        /// <remarks> 
+        public string CertificateName { get; set; }
+
+        /// <summary>
+        /// Web Api base URL
+        /// </summary>
+        public string TodoListBaseAddress { get; set; }
+
+        /// <summary>
+        /// Web Api scope. With client credentials flows, the scopes is ALWAYS of the shape "resource/.default"
+        /// </summary>
+        public string TodoListScope { get; set; }
+
+        /// <summary>
+        /// Reads the configuration from a json file
+        /// </summary>
+        /// <param name="path">Path to the configuration json file</param>
+        /// <returns>AuthenticationConfig read from the json file</returns>
+        public static AuthenticationConfig ReadFromJsonFile(string path)
+        {
+            IConfigurationRoot Configuration;
+
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(path);
+
+            Configuration = builder.Build();
+            return Configuration.Get<AuthenticationConfig>();
+        }
+    }
+
+
+
+}
+
