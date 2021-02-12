516e0d38505ca3426be2073257f17000f77f1dcc
Jean-Marc Prieur <jmprieur@microsoft.com>
2021-02-12 12:10:46 +0100

    Jmprieur/add cert from key vault (#103)
    * Adding code for the KeyVault folder.
    
    * Adding support for a cert.
    
    * Updating the certificate description

---
+++ 3-Using-KeyVault/daemon-console/AuthenticationConfig.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace daemon_console
+{
+    /// <summary>
+    /// Description of the configuration of an AzureAD public client application (desktop/mobile application). This should
+    /// match the application registration done in the Azure portal
+    /// </summary>
+    public class AuthenticationConfig
+    {
+        /// <summary>
+        /// instance of Azure AD, for example public Azure or a Sovereign cloud (Azure China, Germany, US government, etc ...)
+        /// </summary>
+        public string Instance { get; set; } = "https://login.microsoftonline.com/{0}";
+       
+        /// <summary>
+        /// Graph API endpoint, could be public Azure (default) or a Sovereign cloud (US government, etc ...)
+        /// </summary>
+        public string ApiUrl { get; set; } = "https://graph.microsoft.com/";
+
+        /// <summary>
+        /// The Tenant is:
+        /// - either the tenant ID of the Azure AD tenant in which this application is registered (a guid)
+        /// or a domain name associated with the tenant
+        /// - or 'organizations' (for a multi-tenant application)
+        /// </summary>
+        public string Tenant { get; set; }
+
+        /// <summary>
+        /// Guid used by the application to uniquely identify itself to Azure AD
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// URL of the authority
+        /// </summary>
+        public string Authority
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, Instance, Tenant);
+            }
+        }
+
+        /// <summary>
+        /// Name of a certificate in the user certificate store
+        /// </summary>
+        /// <remarks>Daemon applications can authenticate with AAD through two mechanisms: ClientSecret
+        /// (which is a kind of application password: the property above)
+        /// or a certificate previously shared with AzureAD during the application registration 
+        /// (and identified by this CertificateName property)
+        /// <remarks> 
+        public CertificateDescription Certificate { get; set; }
+
+        /// <summary>
+        /// Reads the configuration from a json file
+        /// </summary>
+        /// <param name="path">Path to the configuration json file</param>
+        /// <returns>AuthenticationConfig read from the json file</returns>
+        public static AuthenticationConfig ReadFromJsonFile(string path)
+        {
+            IConfigurationRoot Configuration;
+
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(path);
+
+            Configuration = builder.Build();
+            return Configuration.Get<AuthenticationConfig>();
+        }
+    }
+
+
+
+}
+
---
+++ 3-Using-KeyVault/daemon-console/Program.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates; //Only import this if you are using certificate
+using System.Threading.Tasks;
+
+namespace daemon_console
+{
+    /// <summary>
+    /// This sample shows how to query the Microsoft Graph from a daemon application
+    /// which uses application permissions.
+    /// For more information see https://aka.ms/msal-net-client-credentials
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RunAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+
+            // Load the certificate
+            ICertificateLoader certificateLoader = new DefaultCertificateLoader();
+            certificateLoader.LoadIfNeeded(config.Certificate);
+
+            // Even if this is a console application here, a daemon application is a confidential client application
+            IConfidentialClientApplication app;
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithCertificate(config.Certificate.Certificate)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator. 
+            string[] scopes = new string[] { $"{config.ApiUrl}.default" };
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired");
+                Console.ResetColor();
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+            {
+                // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                // Mitigation: change the scope to be as expected
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Scope provided is not supported");
+                Console.ResetColor();
+            }
+
+            if (result != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+                await apiCaller.CallWebApiAndProcessResultASync($"{config.ApiUrl}v1.0/users", result.AccessToken, Display);
+            }
+        }
+
+        /// <summary>
+        /// Display the result of the Web API call
+        /// </summary>
+        /// <param name="result">Object to display</param>
+        private static void Display(JObject result)
+        {
+            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+            {
+                Console.WriteLine($"{child.Name} = {child.Value}");
+            }
+        }
+    }
+}
