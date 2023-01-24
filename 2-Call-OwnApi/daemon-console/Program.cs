29c6d94e3df530c401d0a3fa77784df160609c2e
Michael Mitchell-Halter <v-michaelmi@microsoft.com>
2023-01-24 11:10:18 -0800

    Removed Inmemory namespace reference

--- 2-Call-OwnApi/daemon-console/Program.cs
+++ 2-Call-OwnApi/daemon-console/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Graph;
 using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
