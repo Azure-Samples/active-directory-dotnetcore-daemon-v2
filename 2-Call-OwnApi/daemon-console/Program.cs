f3700d9696585184cff7328685e673b071676f2d
Jean-Marc Prieur <jmprieur@microsoft.com>
2023-07-13 17:52:05 -0700

    Update to IdWeb 1.13.0

--- 2-Call-OwnApi/daemon-console/Program.cs
+++ 2-Call-OwnApi/daemon-console/Program.cs
@@ -5,11 +5,18 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 using TodoList_WebApi.Models;
+using Microsoft.Extensions.Logging;
 
 // Get the Token acquirer factory instance. By default it reads an appsettings.json
 // file if it exists in the same folder as the app (make sure that the 
 // "Copy to Output Directory" property of the appsettings.json file is "Copy if newer").
 var tokenAcquirerFactory = TokenAcquirerFactory.GetDefaultInstance();
+
+// Add console logging or other services if you wish
+tokenAcquirerFactory.Services.AddLogging(
+    (loggingBuilder) => loggingBuilder.SetMinimumLevel(LogLevel.Warning)
+                                      .AddConsole()
+                                        );
 
 // Create a downstream API service named 'MyApi' which comes loaded with several
 // utility methods to make HTTP calls to the DownstreamApi configurations found
